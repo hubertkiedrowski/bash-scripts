47bcd11ca5ba38482e619367ee2e9588e24377f7
Hubert Kiedrowski <73240170+hubertkiedrowski@users.noreply.github.com>
2023-01-24 23:03:37 +0100

    Create backup installation.sh

---
+++ backup installation.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# microsoft teams #
+
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/ms-teams stable main" > /etc/apt/sources.list.d/teams.list'
+sudo apt update && upgrade
+sudo apt install teams
+
+# vscode #
+
+sudo apt install code -y
+
+sudo apt-get update && upgrade
+
+# microsoft edge # 
+
+wget -O - https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /usr/share/keyrings/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge.list'
+sudo apt update && upgrade
+sudo apt install microsoft-edge-stable
+
+# whatsdesk #
+
+sudo snap install whatsdesk
+sudo apt-get update && upgrade
+
+# discord #
+
+sudo snap install discord
+
+# github #
+type -p curl >/dev/null || sudo apt install curl -y
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
+
+sudo apt-get update && upgrade
+
+sudo snap install notepadqq
+sudo snap install spotify
+sudo snap install audible-for-linux
+
+sudo apt-get update && upgrade
+
